edf983416ec9bfa1ff23b749c344a128bf99b5c2
184381386@qq.com <184381386@qq.com>
2021-10-12 21:34:27 +0800

    框架学习

---
+++ learn/loop/testCls.py
@@ -0,0 +1,17 @@
+class A(object):
+    def m1(self, n):
+        print("self:", self)
+
+    @classmethod
+    def m2(cls, n):
+        print("cls:", cls)
+
+    @staticmethod
+    def m3(cls,n):
+        print("cls:", cls)
+        pass
+
+a = A()
+a.m1(1) # self: <__main__.A object at 0x000001E596E41A90>
+A.m2(1) # cls: <class '__main__.A'>
+A.m3(1,1)
